7ee1b4b576470866e88c9dc44b7718ae751c3c09
Borkanie <43271944+Borkanie@users.noreply.github.com>
2021-09-09 23:07:37 +0300

    Implements simple header

---
+++ JoyStick/src/JoyStick.cpp
@@ -0,0 +1,12 @@
+
+class JoyStick{
+    private:
+        unsigned int xPin;
+        unsigned int yPin;
+        
+
+    public:
+        JoyStick(unsigned int xPin,unsigned int yPin);
+
+        
+};
